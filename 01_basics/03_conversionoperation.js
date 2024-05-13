b24c11692d6ced8faa654a09ba6589f873aa039c
Bharath-kukkunoor <59308629+Bharath-kukkunoor@users.noreply.github.com>
2024-05-13 07:01:22 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score="Bharath"
+// console.log(typeof score);
+
+let valueInNumber=Number(score);
+
+// console.log(typeof valueInNumber);//converison happens to the type specified
+// console.log( valueInNumber);// The actual value returns as NaN(Not a Number)
+
+//if score is assigned as null it returns 0
+//if score is assigned as undefined it returns NaN
+//if score is assigned as string it returns NaN
+//if score is assigned as boolean true/false it returns 1/0
+
+
+// let isLoggedIn=1
+// let isLoggedIn=""//returns false
+// let isLoggedIn="HELLO"//returns true
+
+
+
+// let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber=23
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
